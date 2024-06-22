0e1538479cd015f2cf161764f1bafde9633d57e9
Eduardo23codigo <151943181+Eduardo23codigo@users.noreply.github.com>
2024-06-22 14:52:24 -0400

    segundo

--- proyecto.Web/Startup.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using proyecto.Ioc;
-
-public void ConfigureServices(IServiceCollection services)
-{
-    services.AddProjectDependencies();
-    // otras configuraciones
-}
